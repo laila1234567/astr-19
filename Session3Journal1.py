f7bc437a56c28d50a70c558c79d78a20e51d3204
laila1234567 <96322952+laila1234567@users.noreply.github.com>
2024-10-21 17:43:06 -0700

    Session 3 prompt for Coding Journal 1

---
+++ Session3Journal1.py
@@ -0,0 +1,13 @@
+def f(x):
+    return x**3 + 8
+
+def main():
+    #  x = 9
+    result = f(9)
+    print("The result of f(9) is ", result)
+
+    #Check if result larger than 27
+    if result > 27:
+        print("YAY!")
+
+main()
