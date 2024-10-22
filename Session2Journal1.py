3ba26354a240eaf6e3f524b7e89f053cd6a1770b
laila1234567 <96322952+laila1234567@users.noreply.github.com>
2024-10-21 17:32:05 -0700

    Session 2 prompt for Coding Journal 1

---
+++ Session2Journal1.py
@@ -0,0 +1,18 @@
+float_num1 = 1.5
+float_num2 = 2.5
+int_num1 = 5
+int_num2 = 10
+
+
+sum_result = float_num1 + float_num2
+print("The sum of two floating point numbers is ", sum_result)
+print("The data type of the sum is ", type(sum_result))
+
+
+difference_result = int_num1 - int_num2
+print("\n The difference between two integers is ", difference_result)
+print("The data type of the difference is", type(difference_result))
+
+product_result = float_num1 * int_num2
+print("\n The product of a floating point number and an integer", product_result)
+print("The data type of the product is ", type(product_result))
