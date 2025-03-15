26eaeb64e511e4f8e9b8ede1d62039003cd5f00f
laila1234567 <96322952+laila1234567@users.noreply.github.com>
2025-03-14 22:18:53 -0700

    Session 1 prompt for Coding Journal 1

---
+++ sesh1journal1.py
@@ -0,0 +1,8 @@
+
+print("Full name: Sanjana Sasmal")  
+
+print("Pronouns: she/her") 
+
+print("My favorite movie is Alice in Wonderland because of how colorful and creative it is.")
+
+print("I really enjoy any type of soup because they always give me comfort.")
