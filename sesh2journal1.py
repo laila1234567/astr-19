85131022f2c40429ef7fc87a5babe47a1108f859
laila1234567 <96322952+laila1234567@users.noreply.github.com>
2025-03-14 22:38:04 -0700

    Session 2 prompt for Coding Journal 1

---
+++ sesh2journal1.py
@@ -0,0 +1,16 @@
+float_num1 = 7.8
+float_num2 = 2.9
+int_num1 = 15
+int_num2 = 13
+
+#Sum of two floating point numbers
+sum_result = float_num1 + float_num2
+print("Sum:", sum_result, "Data type:", type(sum_result))
+
+#Difference between two integers
+difference_result = int_num1 - int_num2
+print("Difference:", difference_result, "Data type:", type(difference_result))
+
+#Product of a floating point number and an integer
+product_result = float_num1 * int_num1
+print("Product:", product_result, "Data type:", type(product_result))
